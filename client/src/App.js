ac12237da48f7c8d99665ca4f05a800a73d634f6
AntonSynytsia <anton.synytsia@gmail.com>
2020-03-06 14:32:52 -0800

    Initial setup

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Main from './components/Main';
+
+function App() {
+  return <div></div>;
+}
+
+export default App;
